bcb74e137f3ae39df68c54f7241ec56e5d87330b
kmrnicholson <knichol1@unb.ca>
2016-10-13 16:30:53 -0300

    ayyyy

--- src/cs2043lab5activity3/HelloApp.java
+++ src/cs2043lab5activity3/HelloApp.java
@@ -2,6 +2,6 @@ package cs2043lab5activity3;
 
 public class HelloApp {
 	public static void main(String args[]){
-		System.out.println("Hello Kohdy!");
+		System.out.println("Hello Kohdy Nicholson!");
 	}
 }
